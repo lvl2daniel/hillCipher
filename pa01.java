178823ddb95fd7c85c719374bf99a48538a5f9b3
Daniel Gonzalez <Danielgonzalez4444@yahoo.com>
2023-03-07 22:23:14 -0500

    Close Scanners and Add Try Catch Statements

--- pa01.java
+++ pa01.java
@@ -1,10 +1,5 @@
-import java.io.BufferedReader;
-import java.io.Console;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 /*=============================================================================
@@ -42,97 +37,95 @@ public class pa01 {
         System.out.println();
         String keyFileName = args[0];
         File keyFile = new File(keyFileName);
-        Scanner keyFileScanner = new Scanner(keyFile);
-        String rows = keyFileScanner.next();
-        int matrixSize = Integer.parseInt(rows);
-        int[][] matrix = new int[matrixSize][matrixSize];
-        for (int i = 0; i<matrixSize; i++){
-            for (int k = 0; k<matrixSize; k++){
-            String matrixString = keyFileScanner.next();
-            int number = Integer.parseInt(matrixString);
-            matrix[i][k] = number;
-            }
-
-        }
-        System.out.println("Key matrix:");
-        for (int i = 0; i<matrixSize; i++){
-            for (int k = 0; k < matrixSize; k++){
-            
-                System.out.printf("%4d",  matrix[i][k]);
-                if (k == matrixSize - 1){
-                    System.out.println();
-                }
-                
-            }
-        }
-        System.out.println();
-
-
-
-        //System.out.println(keyFileName);
-        //Scanner plaintextFileInput = new Scanner(System.in);
-        String plaintextFileName = args[1];
-        //System.out.println(plaintextFileName);
-        File inputFile = new File(plaintextFileName);
-
-        // Create a Scanner object to read from the text file
-        Scanner scanner = new Scanner(inputFile, "UTF-8");
-
-        // Use a StringBuilder to accumulate only the alphabetical characters
-        StringBuilder builder = new StringBuilder(100000);
-        while (scanner.hasNext()) {
-            String line = scanner.nextLine();
-            for (char c : line.toCharArray()) {
-                if (c >= 'A' && c <= 'z' && c != '[' && c!= ']' && c!= '_') {
-                    builder.append(c);
-                }
-            }
-        }
-        while ((builder.length() % matrixSize) != 0){
-                builder.append('x');
-            }
-        
-        scanner.close();
-
-        // Convert the StringBuilder to a char array
-        char[] charArray = new char[10000];
-        charArray = builder.toString().toLowerCase().toCharArray();
-        
-        
-
-
-
-        // Print the char array
-        System.out.println("Plaintext:");
-        for (int i = 0; i < charArray.length; i++) {
-			System.out.print(charArray[i]);
-			if ((i + 1) % 80 == 0) {
-				System.out.println();
-			}
-		}
-        System.out.println();
-        String cipherText = "";
-        for (int i = 0; i < charArray.length; i+= matrixSize){
-            int[] plainArray = new int[matrixSize];
-            int[] cipherArray = new int[matrixSize];
-            for (int j = 0; j < matrixSize; j++) {
-				plainArray[j] = charArray[i+j] - 'a';
-			}
-            for (int j = 0; j<matrixSize; j++){
+        try (Scanner keyFileScanner = new Scanner(keyFile)) {
+            String rows = keyFileScanner.next();
+            int matrixSize = Integer.parseInt(rows);
+            int[][] matrix = new int[matrixSize][matrixSize];
+            for (int i = 0; i<matrixSize; i++){
                 for (int k = 0; k<matrixSize; k++){
-                    cipherArray[j] += matrix[j][k] * plainArray[k];
+                String matrixString = keyFileScanner.next();
+                int number = Integer.parseInt(matrixString);
+                matrix[i][k] = number;
                 }
-                cipherArray[j] %= 26;
-                cipherText += (char)(cipherArray[j] + 'a');
+
             }
-        }
-        System.out.println();
-        System.out.println("Ciphertext:");
-        for (int i = 0; i < cipherText.length(); i++) {
-			System.out.print(cipherText.charAt(i));
-			if ((i + 1) % 80 == 0) {
-				System.out.println();
-			}
+            System.out.println("Key matrix:");
+            for (int i = 0; i<matrixSize; i++){
+                for (int k = 0; k < matrixSize; k++){
+                
+                    System.out.printf("%4d",  matrix[i][k]);
+                    if (k == matrixSize - 1){
+                        System.out.println();
+                    }
+                    
+                }
+            }
+            System.out.println();
+            String plaintextFileName = args[1];
+            File inputFile = new File(plaintextFileName);
+
+            // Create a Scanner object to read from the text file
+            Scanner scanner = new Scanner(inputFile, "UTF-8");
+
+            // Use a StringBuilder to accumulate only the alphabetical characters
+            StringBuilder builder = new StringBuilder(100000);
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                for (char c : line.toCharArray()) {
+                    if (c >= 'A' && c <= 'z' && c != '[' && c!= ']' && c!= '_') {
+                        builder.append(c);
+                    }
+                }
+            }
+            while ((builder.length() % matrixSize) != 0){
+                    builder.append('x');
+                }
+            
+            scanner.close();
+
+            // Convert the StringBuilder to a char array
+            char[] charArray = new char[10000];
+            charArray = builder.toString().toLowerCase().toCharArray();
+            
+            
+
+
+
+            // Print the char array
+            System.out.println("Plaintext:");
+            for (int i = 0; i < charArray.length; i++) {
+            	System.out.print(charArray[i]);
+            	if ((i + 1) % 80 == 0) {
+            		System.out.println();
+            	}
+            }
+            System.out.println();
+            String cipherText = "";
+            for (int i = 0; i < charArray.length; i+= matrixSize){
+                int[] plainArray = new int[matrixSize];
+                int[] cipherArray = new int[matrixSize];
+                for (int j = 0; j < matrixSize; j++) {
+            		plainArray[j] = charArray[i+j] - 'a';
+            	}
+                for (int j = 0; j<matrixSize; j++){
+                    for (int k = 0; k<matrixSize; k++){
+                        cipherArray[j] += matrix[j][k] * plainArray[k];
+                    }
+                    cipherArray[j] %= 26;
+                    cipherText += (char)(cipherArray[j] + 'a');
+                }
+            }
+            System.out.println();
+            System.out.println("Ciphertext:");
+            for (int i = 0; i < cipherText.length(); i++) {
+            	System.out.print(cipherText.charAt(i));
+            	if ((i + 1) % 80 == 0) {
+            		System.out.println();
+            	}
+            }
+        } catch (NumberFormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
         System.out.println();
     }
